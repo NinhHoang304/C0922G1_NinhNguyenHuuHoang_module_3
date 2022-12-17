25d260c9a77f0d9ab010c4b1c8e5d596d8623002
NinhHoang304 <hoangninh3004@gmail.com>
2022-12-18 02:55:03 +0700

    fix font

--- case_study/database/query_06_10.sql
+++ case_study/database/query_06_10.sql
@@ -57,7 +57,7 @@ order by thang asc;
 -- 10.Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
 -- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem 
 -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
-SELECT 
+select 
     hd.ma_hop_dong,
     hd.ngay_lam_hop_dong,
     hd.ngay_ket_thuc,
