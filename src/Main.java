219d18b0b6d84adc0f907aedafca08d54afc1e82
NinhHoang304 <hoangninh3004@gmail.com>
2022-12-29 11:34:42 +0700

    cai dat tomcat tao project moi

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào 1 số nguyên chẵn: ");
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int l = 0; l < n - i * 2; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
