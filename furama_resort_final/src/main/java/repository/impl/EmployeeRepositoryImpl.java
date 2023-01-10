a18f218fe450ced244089fcda6a3c8fb1b44ee6b
NinhHoang304 <hoangninh3004@gmail.com>
2023-01-10 16:51:56 +0700

    fix

--- furama_resort_final/src/main/java/repository/impl/EmployeeRepositoryImpl.java
+++ furama_resort_final/src/main/java/repository/impl/EmployeeRepositoryImpl.java
@@ -4,6 +4,7 @@ import model.Employee;
 import repository.BaseRepository;
 import repository.IEmployeeRepository;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
